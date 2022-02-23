2c624ef08635f9333540d9d92be8b4561d699a8d
Heidi Snell <hms8694@gmail.com>
2022-02-22 23:53:29 -0600

    wip on main.java

--- Main.java
+++ Main.java
@@ -16,15 +16,16 @@ public class Main {
         Minefield minefield = new Minefield(size, numMines);
 
         // keep digging until win or hit a bomb
+        Scanner digScanner = new Scanner(System.in);
         while (minefield.dug.size() < (Math.pow(size, 2) - numMines)) {
-            Scanner digScanner = new Scanner(System.in);
-            System.out.println("Next place to dig? Input as row, col: ");
+            System.out.println("Where do you want to dig next? Input as row, col: ");
             String userInputString = digScanner.nextLine();
-            digScanner.close();
 
             int[] userInput = Arrays.stream(userInputString.split(",")).mapToInt(Integer::parseInt).toArray();
 
             System.out.println(userInput);
         }
+        digScanner.close();
+
     }
 }
