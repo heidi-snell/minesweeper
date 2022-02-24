7426eab1b4ff343297c5ac87841c5e938b05d767
Heidi Snell <hms8694@gmail.com>
2022-02-23 22:21:25 -0600

    turn string input into two ints

--- Main.java
+++ Main.java
@@ -20,9 +20,13 @@ public class Main {
             scanner.nextLine();
             String userInputString = scanner.nextLine();
 
-            int[] userInput = Arrays.stream(userInputString.split(",")).mapToInt(Integer::parseInt).toArray();
+            String[] splitArray = userInputString.split(",");
+            int[] userInput = new int[splitArray.length];
+            for (int i = 0; i < splitArray.length; i++) {
+                userInput[i] = Integer.parseInt(splitArray[i]);
+            }
 
-            System.out.println(userInput);
+            System.out.println(userInput[0] + " " + userInput[1]);
         }
         scanner.close();
 
